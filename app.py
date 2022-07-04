cbdd50da9112f01b822c3ffaa1fe776879c39e47
rherath <90292327+rherath@users.noreply.github.com>
2022-07-04 18:50:25 +1000

    Update app.py
    Ravi

--- app.py
+++ app.py
@@ -4,3 +4,8 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Please subscribe, like, and comment on this video, TY!!!'
+
+>>> year = 2016
+>>> event = 'Referendum'
+>>> f'Results of the {year} {event}'
+'Results of the 2016 Referendum'
